91debcba09ff29628dba32bb7fe947c9496c0d8c
nathanturnis <nturnis2@gmail.com>
2023-10-16 12:23:54 -0500

    can now remove items from cart

--- midterm/cart.js
+++ midterm/cart.js
@@ -3,22 +3,34 @@ getCart();
 var countText = document.getElementById("cart-counter");
 countText.innerHTML = productsInCart;
 
+var itemsInCartText = document.getElementById("items-in-cart");
+
+if (itemsInCartText != null) {
+    itemsInCartText.innerHTML = `Items in cart: ${productsInCart}`;
+}
+
 //adds a product to the cart
 function addToCart() {
     productsInCart++;
     countText.innerHTML = productsInCart;
+    storeCart();
 }
 
 //remove a product from the cart 
 function removeFromCart() {
+    if (productsInCart <= 0) return;
     productsInCart--;
     countText.innerHTML = productsInCart;
+    itemsInCartText.innerHTML = `Items in cart: ${productsInCart}`;
+    storeCart();
 }
 
+//stores product count to local storage
 function storeCart() {
     localStorage.setItem("cartCount", productsInCart);
 }
 
+//gets the product count from local storage
 function getCart() {
     productsInCart = localStorage.getItem("cartCount");
     if (productsInCart == null) {
