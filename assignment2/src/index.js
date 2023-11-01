a43a3632aae56f652246757084a046623588da3c
nathanturnis <nturnis2@gmail.com>
2023-11-01 09:22:44 -0500

    doing some testing

--- assignment2/src/index.js
+++ assignment2/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Test, Test2 } from './script.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Test />
+    <Test2 />
   </React.StrictMode>
 );
---
+++ assignment2/src/script.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Test = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export const Test2 = () => {
+    return (
+        <div>
+            <h1>Test2</h1>
+        </div>
+    );
+}
