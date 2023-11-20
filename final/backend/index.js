d518f9fd926ab72b750f46eb65623ce1683e89de
nathanturnis <nturnis2@gmail.com>
2023-11-20 13:22:37 -0600

    added node

---
+++ final/backend/index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+
+const { MongoClient } = require("mongodb");
+// Mongo
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "se319final";
+const client = new MongoClient(url);
+const db = client.db(dbName);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = "8081";
+const host = "localhost";
+
+app.listen(port, () => {
+    console.log("App listening at http://%s:%s", host, port);
+});
+
+app.get("/allProducts", async (req, res) => {
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
+    const query = {};
+    const results = await db
+        .collection("products")
+        .find(query)
+        .limit(100);
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
