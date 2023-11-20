e3915866ea649ea7553e3be28da0b2d8a343449d
nathanturnis <nturnis2@gmail.com>
2023-11-20 12:26:03 -0600

    adding navbar

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -27,17 +27,18 @@ function App() {
                 <a className="nav-link">About</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
+            <form className="d-flex" role="search" id="product-search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
             <div className="d-flex">
-              test
+              <a><img src="./images/cart-icon.png" width="44px" className="ms-1 me-4" /></a>
+              <div id="cart-counter">0</div>
             </div>
           </div>
         </div>
-      </nav>
-    </div>
+      </nav >
+    </div >
   );
 }
 
--- final-project/src/index.js
+++ final-project/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+import './app.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
