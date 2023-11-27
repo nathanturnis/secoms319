a345b2fe0805cf049a97699ef63d4e2f00df7465
nathanturnis <nturnis2@gmail.com>
2023-11-27 10:58:31 -0600

    payment info

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -12,6 +12,14 @@ function App() {
   const [cart, setCart] = useState([]);
   const [filteredCart, setFilteredCart] = useState([]);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [card, setCard] = useState("");
+  const [address, setAddress] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+
   useEffect(() => {
     getProducts();
   }, []);
@@ -174,6 +182,77 @@ function App() {
     document.getElementById("selected-category").classList.remove("visible");
     document.getElementById("selected-category").classList.add("invisible");
     document.getElementById("category-select").firstChild.value = "";
+  }
+
+  const isNumeric = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+  }
+
+  const cardInputDashes = (e) => {
+    e.value = e.value.replace(/-/g, '');
+    let newVal = '';
+    for (var i = 0, nums = 0; i < e.value.length; i++) {
+      if (nums != 0 && nums % 4 == 0) {
+        newVal += '-';
+      }
+      newVal += e.value[i]
+      if (isNumeric(e.value[i])) {
+        nums++;
+      }
+    }
+    e.value = newVal;
+  }
+
+  const validate = (e) => {
+    let isValidated = true;
+    let nameInput = document.getElementById("inputName");
+    let emailInput = document.getElementById("inputEmail4");
+    let cardInput = document.getElementById("inputCard");
+
+    if (cartTotal <= 0) {
+      document.getElementById("cartAlert").innerHTML = `<div class="alert alert-danger" role="alert" id="alertCart">No items in the cart!</div>`;
+      return;
+    }
+
+    //validate name
+    if (name.length <= 0) {
+      nameInput.setAttribute("class", "form-control is-invalid");
+      isValidated = false;
+    } else {
+      nameInput.setAttribute("class", "form-control is-valid")
+    }
+
+    //validate email
+    if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )) {
+      emailInput.setAttribute("class", "form-control is-invalid");
+      isValidated = false;
+    } else {
+      emailInput.setAttribute("class", "form-control is-valid");
+    }
+
+    //validate card
+    if (!card.match(/^[0-9]{4}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}$/)) {
+      cardInput.setAttribute("class", "form-control is-invalid");
+      isValidated = false;
+    } else {
+      cardInput.setAttribute("class", "form-control is-valid")
+    }
+
+    //open modal if validated
+    if (isValidated) {
+      //   setPageState(2);
+    }
+  }
+
+  const displayInformation = () => {
+    return (
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Name: {name}</li>
+        <li class="list-group-item">Email: {email}</li>
+        <li class="list-group-item">Card: XXXX-XXXX-XXXX-{card.slice(-4)}</li>
+        <li class="list-group-item">Address: {address}, {city}, {state} {zipCode}</li>
+      </ul>);
   }
 
   //PRODUCTS PAGE
@@ -367,6 +446,84 @@ function App() {
               </tbody>
             </table>
           </div>
+
+          <h2 className="mt-3 mb-3">Payment Information</h2>
+
+          <form className="row g-3 mb-5" id="checkout-form">
+            <div className="col-md-6">
+              <label for="inputName" className="form-label">Full Name</label>
+              <input type="text" className="form-control" id="inputName" onChange={(e) => { setName(e.target.value) }} />
+              <div className="valid-feedback">Looks good!</div>
+              <div className="invalid-feedback">Must be like, "John Doe"</div>
+            </div>
+
+            <div className="col-md-6">
+              <label for="inputEmail4" className="form-label">Email</label>
+              <input type="email" className="form-control" id="inputEmail4" onChange={(e) => { setEmail(e.target.value) }} />
+              <div className="valid-feedback">Looks good!</div>
+              <div className="invalid-feedback">Must be like, "abc@xyz.efg"</div>
+            </div>
+
+            <div className="col-12">
+              <label for="inputCard" className="form-label">Card</label>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1"
+                ><i className="bi-credit-card-fill"></i></span>
+                <input
+                  type="text"
+                  id="inputCard"
+                  className="form-control"
+                  placeholder="XXXX-XXXX-XXXX-XXXX"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  maxLength={19}
+                  onChange={(e) => { setCard(e.target.value); cardInputDashes(e.target); }} />
+                <div className="valid-feedback">Looks good!</div>
+                <div className="invalid-feedback">
+                  Must be like, "7777-7777-7777-7777"
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12">
+              <label for="inputAddress" className="form-label">Address</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress"
+                placeholder="1234 Main St" onChange={(e) => { setAddress(e.target.value) }} />
+            </div>
+            <div className="col-12">
+              <label for="inputAddress2" className="form-label">Address 2</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress2"
+                placeholder="Apartment, studio, or floor" />
+            </div>
+            <div className="col-md-6">
+              <label for="inputCity" className="form-label">City</label>
+              <input type="text" className="form-control" id="inputCity" onChange={(e) => { setCity(e.target.value) }} />
+            </div>
+            <div className="col-md-4">
+              <label for="inputState" className="form-label">State</label>
+              <select id="inputState" className="form-select" onChange={(e) => { setState(e.target.value) }}>
+                <option selected>Choose...</option>
+                <option>Iowa</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="col-md-2">
+              <label for="inputZip" className="form-label">Zip</label>
+              <input type="text" className="form-control" id="inputZip" onChange={(e) => { setZipCode(e.target.value) }} />
+            </div>
+            <div className="col-12">
+              <button type="button" className="btn btn-success" onClick={(e) => { validate(e); }}>
+                <i className="bi-bag-check"></i> Order
+              </button>
+            </div>
+          </form>
+
         </div>
 
       </div>
